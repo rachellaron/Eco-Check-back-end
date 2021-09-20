4d09bcb88fa69974e6ab1d11f929df93ffd6c82b
Rachel <racharon@gmail.com>
2021-09-20 18:07:38 +1000

    updated label tips css

--- client/src/components/LableTips.js
+++ client/src/components/LableTips.js
@@ -17,16 +17,15 @@ function TipsToggle() {
 
         <div className="label-tips">
             <label>Lable Tips</label>
-            <button className="dropdown-btn" onClick={() => setShow(!show)} ><div className="menu-icon" onClick={handleClick}>
+            <button className="dropdown-btn" onClick={() => setShow(!show)} >
+                <div className="dropdown-btn" onClick={handleClick}>
                     <i className={click ? 'fas fa-times' : 'fas fa-chevron-down'} />
                 </div></button>
-
-            <div className="lable-tips-open">
                 {
                     show ? <img className="label-tips-img" src={LabelTipsTrans} alt="" /> : null
                 }
 
-            </div>
+            
         </div>
     );
 }
